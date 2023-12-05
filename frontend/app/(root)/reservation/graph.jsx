f607d52521b83185f3a22ebe58b93abd0e9542af
judycpc <abc0952753111@gmail.com>
2023-12-06 00:44:30 +0800

    reservation rwd

--- frontend/app/(root)/reservation/graph.jsx
+++ frontend/app/(root)/reservation/graph.jsx
@@ -7,15 +7,15 @@ const Graph = ({ data, current }) => {
   const spaces = data[current]
 
   return (
-    <div className="rounded-3xl bg-neutral-200 p-5 flex-1 flex flex-col justify-center">
+    <div className="rounded-3xl bg-neutral-200 p-3 lg:p-5 flex-1 flex flex-col justify-center">
       <RadioGroup className="flex justify-between items-center">
         <div className="flex flex-col">
           {
             Object.keys(spaces).slice(0, 6).map((key) => (
               <div key={key}>
                 <RadioGroupItem value={key} id={key} className="peer sr-only" disabled={!spaces[key]} />
-                <Label htmlFor={key} className="w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center py-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
-                  <p className="text-32">{key}</p>
+                <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                  <p className="text-[18px] lg:text-32">{key}</p>
                 </Label>
               </div>
             ))
@@ -27,8 +27,8 @@ const Graph = ({ data, current }) => {
               Object.keys(spaces).slice(6, 10).map((key) => (
                 <div key={key}>
                   <RadioGroupItem value={key} id={key} className="peer sr-only" disabled={!spaces[key]} />
-                  <Label htmlFor={key} className="w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center py-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
-                    <p className="text-32">{key}</p>
+                  <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                    <p className="text-[18px] lg:text-32">{key}</p>
                   </Label>
                 </div>
               ))
@@ -39,8 +39,8 @@ const Graph = ({ data, current }) => {
               Object.keys(spaces).slice(10, 14).map((key) => (
                 <div key={key}>
                   <RadioGroupItem value={key} id={key} className="peer sr-only" disabled={!spaces[key]} />
-                  <Label htmlFor={key} className="w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center py-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
-                    <p className="text-32">{key}</p>
+                  <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                    <p className="text-[18px] lg:text-32">{key}</p>
                   </Label>
                 </div>
               ))
@@ -52,8 +52,8 @@ const Graph = ({ data, current }) => {
             Object.keys(spaces).slice(14, 20).map((key) => (
               <div key={key}>
                 <RadioGroupItem value={key} id={key} className="peer sr-only" disabled={!spaces[key]} />
-                <Label htmlFor={key} className="w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center py-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
-                  <p className="text-32">{key}</p>
+                <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                  <p className="text-[18px] lg:text-32">{key}</p>
                 </Label>
               </div>
             ))
