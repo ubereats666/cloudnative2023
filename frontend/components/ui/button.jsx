ce51a55e9b6a63f80c6f9324732138d2ff016f86
Xiugapurin <jason32567810@gmail.com>
2023-12-13 20:48:27 +0800

    revert code to previous commit

--- frontend/components/ui/button.jsx
+++ frontend/components/ui/button.jsx
@@ -11,15 +11,23 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+        primary:
+          "bg-primary text-gray-1 hover:bg-primary/90 dark:bg-gray-1 dark:text-primary dark:hover:bg-gray-1/90",
+        secondary:
+          "bg-secondary text-slate-900 hover:bg-secondary/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+        white:
+          "bg-white text-t-paragraph hover:bg-white/90 dark:bg-t-paragraph dark:text-white dark:hover:bg-t-paragraph/90 shadow-md",
+        link: "text-primary underline-offset-4 hover:underline dark:text-t-invert",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
         ghost:
-          "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+          "text-t-title hover:bg-gray-1 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+        setting:
+          "border-2 border-[#75B066] bg-[#75B066] text-gray-1 rounded-full px-6 py-2",
+        setting_outline:
+          "border-2 border-[#75B066] text-[#75B066] rounded-full px-6 py-2",
       },
       size: {
         default: "rounded-md px-3 py-2 lg:px-5",
@@ -39,12 +47,24 @@ const buttonVariants = cva(
 );
 
 const Button = React.forwardRef(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      text,
+      asChild = false,
+      onClick = null,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={onClick}
         {...props}
       />
     );
