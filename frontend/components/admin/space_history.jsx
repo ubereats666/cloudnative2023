fbe3ec098994afdfc86657e8145144a374f39220
tingyo123 <tingyo128@gmail.com>
2023-12-13 17:12:59 +0800

    merge

---
+++ frontend/components/admin/space_history.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import useFetch from "@/hooks/useFetch";
+
+const SpaceHistory = () => {
+  const { data, isLoading, error } = useFetch("get_space_history");
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
+  function convertToHoursAndMinutes(durationInMinutes) {
+    const hours = Math.floor(durationInMinutes / 60); // 取得小時數
+    const minutes = durationInMinutes % 60; // 取得剩餘的分鐘數
+
+    const formattedHours = String(hours).padStart(2, "0"); // 轉換成兩位數的小時
+    const formattedMinutes = String(minutes).padStart(2, "0"); // 轉換成兩位數的分鐘
+
+    return `${formattedHours}h${formattedMinutes}m`; // 回傳格式化後的字串
+  }
+
+  const numOfRecords = data.length;
+
+  return (
+    <div className="flex flex-row flex-1 grow bg-danger">
+      <div className="flex flex-col flex-1 items-center justify-center px-4 bg-amber-300">
+        <p className="text-[60px] text-green-4">{numOfRecords}</p>
+        <p className="text-[30px] text-gray-6">records</p>
+      </div>
+      <div className="overflow-y-auto max-h-[216px]">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50 sticky top-0">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                車牌號碼
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                進場時間
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                離場時間
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                停放時長
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {data.map((row, index) => (
+              <tr key={index} className="text-center">
+                <td className="px-6 py-4 whitespace-nowrap">{row.plate}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {row.enter_time}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{row.exit_time}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {convertToHoursAndMinutes(row.duration)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default SpaceHistory;
--- frontend/components/ui/button.jsx
+++ frontend/components/ui/button.jsx
@@ -1,42 +1,29 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center text-14 font-bold whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
   {
     variants: {
       variant: {
-        default:
-          "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
-        primary:
-          "bg-primary text-gray-1 hover:bg-primary/90 dark:bg-gray-1 dark:text-primary dark:hover:bg-gray-1/90",
-        secondary:
-          "bg-secondary text-slate-900 hover:bg-secondary/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
-        white:
-          "bg-white text-t-paragraph hover:bg-white/90 dark:bg-t-paragraph dark:text-white dark:hover:bg-t-paragraph/90 shadow-md",
-        link: "text-primary underline-offset-4 hover:underline dark:text-t-invert",
+        default: "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        ghost:
-          "text-t-title hover:bg-gray-1 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        setting:
-          "border-2 border-[#75B066] bg-[#75B066] text-gray-1 rounded-full px-6 py-2",
-        setting_outline:
-          "border-2 border-[#75B066] text-[#75B066] rounded-full px-6 py-2",
+        secondary:
+          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+        ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
-        default: "rounded-md px-3 py-2",
-        square: "rounded p-2",
-        md: "w-full px-6 py-3 rounded-md",
-        lg: "rounded-md px-8 py-2",
-        full: "rounded-full px-3 py-1 md:px-3 md:py-2",
-        none: "p-none",
-        icon: "w-10",
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
@@ -44,32 +31,17 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-);
+)
 
-const Button = React.forwardRef(
-  (
-    {
-      className,
-      variant,
-      size,
-      text,
-      asChild = false,
-      onClick = null,
-      ...props
-    },
-    ref
-  ) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        onClick={onClick}
-        {...props}
-      />
-    );
-  }
-);
-Button.displayName = "Button";
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />)
+  );
+})
+Button.displayName = "Button"
 
-export { Button, buttonVariants };
+export { Button, buttonVariants }
