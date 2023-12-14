2b9ceca0c048722fb896b8b14f501d6e7eff94e0
judycpc <abc0952753111@gmail.com>
2023-12-14 20:29:24 +0800

    fix reservation graph number

--- frontend/app/(root)/reservation/graph.jsx
+++ frontend/app/(root)/reservation/graph.jsx
@@ -4,6 +4,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const Graph = ({ data, current, setSelected }) => {
   const spaces = data[current];
+  const floor_name = { 1: "2", 2: "1", 3: "B1", 4: "B2" }
 
   return (
     <div className="rounded-3xl bg-neutral-200 p-3 lg:p-5 flex-1 flex flex-col justify-center">
@@ -20,7 +21,7 @@ const Graph = ({ data, current, setSelected }) => {
               <div key={key}>
                 <RadioGroupItem
                   value={
-                    current + (parseInt(key) + 1).toString().padStart(2, "0")
+                    floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")
                   }
                   id={key}
                   className="peer sr-only"
@@ -31,7 +32,7 @@ const Graph = ({ data, current, setSelected }) => {
                   className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
                 >
                   <p className="text-[18px] lg:text-32">
-                    {current + (parseInt(key) + 1).toString().padStart(2, "0")}
+                    {floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")}
                   </p>
                 </Label>
               </div>
@@ -45,7 +46,7 @@ const Graph = ({ data, current, setSelected }) => {
                 <div key={key}>
                   <RadioGroupItem
                     value={
-                      current + (parseInt(key) + 1).toString().padStart(2, "0")
+                      floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")
                     }
                     id={key}
                     className="peer sr-only"
@@ -56,7 +57,7 @@ const Graph = ({ data, current, setSelected }) => {
                     className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
                   >
                     <p className="text-[18px] lg:text-32">
-                      {current +
+                      {floor_name[current] +
                         (parseInt(key) + 1).toString().padStart(2, "0")}
                     </p>
                   </Label>
@@ -70,7 +71,7 @@ const Graph = ({ data, current, setSelected }) => {
                 <div key={key}>
                   <RadioGroupItem
                     value={
-                      current + (parseInt(key) + 1).toString().padStart(2, "0")
+                      floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")
                     }
                     id={key}
                     className="peer sr-only"
@@ -81,7 +82,7 @@ const Graph = ({ data, current, setSelected }) => {
                     className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
                   >
                     <p className="text-[18px] lg:text-32">
-                      {current +
+                      {floor_name[current] +
                         (parseInt(key) + 1).toString().padStart(2, "0")}
                     </p>
                   </Label>
@@ -96,7 +97,7 @@ const Graph = ({ data, current, setSelected }) => {
               <div key={key}>
                 <RadioGroupItem
                   value={
-                    current + (parseInt(key) + 1).toString().padStart(2, "0")
+                    floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")
                   }
                   id={key}
                   className="peer sr-only"
@@ -107,7 +108,7 @@ const Graph = ({ data, current, setSelected }) => {
                   className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
                 >
                   <p className="text-[18px] lg:text-32">
-                    {current + (parseInt(key) + 1).toString().padStart(2, "0")}
+                    {floor_name[current] + (parseInt(key) + 1).toString().padStart(2, "0")}
                   </p>
                 </Label>
               </div>
