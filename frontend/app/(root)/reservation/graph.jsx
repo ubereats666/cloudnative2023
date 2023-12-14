dc27e104dbaca2190627bc7e04657fe30fda1f3c
Xiugapurin <jason32567810@gmail.com>
2023-12-14 17:59:35 +0800

    change the render method of Reservation Page

--- frontend/app/(root)/reservation/graph.jsx
+++ frontend/app/(root)/reservation/graph.jsx
@@ -1,97 +1,121 @@
 import React from "react";
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const Graph = ({ data, current, setSelected }) => {
-  const spaces = data[current]
+  const spaces = data[current];
 
   return (
     <div className="rounded-3xl bg-neutral-200 p-3 lg:p-5 flex-1 flex flex-col justify-center">
       <RadioGroup
         className="flex justify-between items-center"
-        onValueChange={(value) => { setSelected(value) }}
+        onValueChange={(value) => {
+          setSelected(value);
+        }}
       >
         <div className="flex flex-col">
-          {
-            Object.keys(spaces).slice(0, 6).map((key) => (
+          {Object.keys(spaces)
+            .slice(0, 6)
+            .map((key) => (
               <div key={key}>
                 <RadioGroupItem
-                  value={current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                  value={
+                    current + (parseInt(key) + 1).toString().padStart(2, "0")
+                  }
                   id={key}
                   className="peer sr-only"
                   disabled={!spaces[key]}
                 />
-                <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                <Label
+                  htmlFor={key}
+                  className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
+                >
                   <p className="text-[18px] lg:text-32">
-                    {current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                    {current + (parseInt(key) + 1).toString().padStart(2, "0")}
                   </p>
                 </Label>
               </div>
-            ))
-          }
+            ))}
         </div>
         <div className="flex">
           <div className="flex flex-col">
-            {
-              Object.keys(spaces).slice(6, 10).map((key) => (
+            {Object.keys(spaces)
+              .slice(6, 10)
+              .map((key) => (
                 <div key={key}>
                   <RadioGroupItem
-                    value={current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                    value={
+                      current + (parseInt(key) + 1).toString().padStart(2, "0")
+                    }
                     id={key}
                     className="peer sr-only"
                     disabled={!spaces[key]}
                   />
-                  <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                  <Label
+                    htmlFor={key}
+                    className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
+                  >
                     <p className="text-[18px] lg:text-32">
-                      {current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                      {current +
+                        (parseInt(key) + 1).toString().padStart(2, "0")}
                     </p>
                   </Label>
                 </div>
-              ))
-            }
+              ))}
           </div>
           <div className="flex flex-col">
-            {
-              Object.keys(spaces).slice(10, 14).map((key) => (
+            {Object.keys(spaces)
+              .slice(10, 14)
+              .map((key) => (
                 <div key={key}>
                   <RadioGroupItem
-                    value={current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                    value={
+                      current + (parseInt(key) + 1).toString().padStart(2, "0")
+                    }
                     id={key}
                     className="peer sr-only"
                     disabled={!spaces[key]}
                   />
-                  <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                  <Label
+                    htmlFor={key}
+                    className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
+                  >
                     <p className="text-[18px] lg:text-32">
-                      {current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                      {current +
+                        (parseInt(key) + 1).toString().padStart(2, "0")}
                     </p>
                   </Label>
                 </div>
-              ))
-            }
+              ))}
           </div>
         </div>
         <div className="flex flex-col">
-          {
-            Object.keys(spaces).slice(14, 20).map((key) => (
+          {Object.keys(spaces)
+            .slice(14, 20)
+            .map((key) => (
               <div key={key}>
                 <RadioGroupItem
-                  value={current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                  value={
+                    current + (parseInt(key) + 1).toString().padStart(2, "0")
+                  }
                   id={key}
                   className="peer sr-only"
                   disabled={!spaces[key]}
                 />
-                <Label htmlFor={key} className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]">
+                <Label
+                  htmlFor={key}
+                  className="lg:w-28 bg-[#CFE8C6] peer-disabled:bg-neutral-400 rounded text-[#75B066] peer-disabled:text-neutral-300 flex justify-center px-1 py-2 lg:p-2 border-4 border-neutral-200 peer-data-[state=checked]:border-[#75B066]"
+                >
                   <p className="text-[18px] lg:text-32">
-                    {current + (parseInt(key) + 1).toString().padStart(2, '0')}
+                    {current + (parseInt(key) + 1).toString().padStart(2, "0")}
                   </p>
                 </Label>
               </div>
-            ))
-          }
+            ))}
         </div>
       </RadioGroup>
     </div>
-  )
+  );
 };
 
 export default Graph;
